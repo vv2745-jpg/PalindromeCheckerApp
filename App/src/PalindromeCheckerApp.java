a5098496c8794d309e56cb2a5e9c93656158619b
vv2745-jpg <vv2745@srmist.edu.in>
2026-02-26 10:31:37 +0530

    [vanamaGowtham] Added UC1: Application Entry & Welcome Message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,44 @@
+
+/**
+ * ================================================================
+ * MAIN CLASS - UseCase1PalindromeApp
+ * ================================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No palindrome logic is implemented yet.
+ *
+ * The goal is to establish a clear startup flow.
+ *
+ * @author vanamaGowtham
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point.
+     * <p>
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+      public static void main(System[] args) {
+        System.out.println("welcome to palindrome checker app");
+        System.out.println("version: 1.0");
+        System.out.println("System intialized successfully");
+
+
+    }
+}
+
