21d778fc6141476a5754d04669d7e406c1701aab
vv2745-jpg <vv2745@srmist.edu.in>
2026-04-07 16:49:24 +0530

    Gowtham Added UC11-Object-Oriented

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
-public class PalindroneCheckerApp {
-    public static boolean checkPalindrome(String input) {
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i))
+class PalindromeService {
+    public boolean checkPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end))
                 return false;
+            start++;
+            end--;
         }
         return true;
     }
+}
+public class PalindroneCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input: ");
         String input = sc.nextLine();
-        boolean result = checkPalindrome(input);
+        PalindromeService service = new PalindromeService();
+        boolean result = service.checkPalindrome(input);
         System.out.println("Is Palindrome?: " + result);
     }
 }
